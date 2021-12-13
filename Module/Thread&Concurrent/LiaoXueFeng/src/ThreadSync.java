3c539f6b576733562fe8dbabb0715860fb1e117a
xyt <544251790@qq.com>
2021-12-14 01:25:34 +0800

    learn synchronize and lock

---
+++ Module/Thread&Concurrent/LiaoXueFeng/src/ThreadSync.java
@@ -0,0 +1,42 @@
+/**
+ * @author xiayitian
+ * @date 2021/12/14 0:58
+ */
+public class ThreadSync{
+    public static void main(String[] args) throws Exception {
+        var add = new AddThread();
+        var dec = new DecThread();
+        add.start();
+        dec.start();
+        add.join();
+        dec.join();
+        System.out.println(Counter.count);
+    }
+}
+
+class Counter {
+    public static final Object lock = new Object();
+//    public static final Object lock1 = new Object(); mistake
+//    public static final Object lock2 = new Object();
+    public static int count = 0;
+}
+
+class AddThread extends Thread {
+    public void run() {
+        for (int i=0; i<10000; i++) {
+            synchronized(Counter.lock) {
+                Counter.count += 1;
+            }
+        }
+    }
+}
+
+class DecThread extends Thread {
+    public void run() {
+        for (int i=0; i<10000; i++) {
+            synchronized(Counter.lock) {
+                Counter.count -= 1;
+            }
+        }
+    }
+}
