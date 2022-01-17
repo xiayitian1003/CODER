9153a9d3938bf07243baa3cdea8682de209a088a
xyt <544251790@qq.com>
2022-01-18 00:44:50 +0800

    add Unit2.py

---
+++ Python/MOOCPython/Unit2.py
@@ -0,0 +1,14 @@
+# n = input()
+# print(2 ** int(n))
+
+# args = input().split()
+# x, a, b, c, d = float(args[0]), float(args[1]), float(args[2]), float(args[3]), float(args[4])
+# fx = a * x ** 3 + b * x ** 2 + c * x + d
+# # print(fx)
+# print("%.7f" % fx)
+
+# a = int(input())
+# if a % 2 == 0:
+#     print("even")
+# else:
+#     print("odd")
