0ca9fdf79738098b30b18521367857344af6b33f
yitian.xia <yitian.xia@transsion.com>
2022-01-17 21:16:15 +0800

    finish Unit1 of MOOCPython

---
+++ Python/MOOCPython/Unit1.py
@@ -0,0 +1,73 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+# def print_hi(name):
+#     # Use a breakpoint in the code line below to debug your script.
+#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#
+#
+# # Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# print(1,2,3,end="")
+# print("ok")
+# y = "请输入你的名字:"
+# x = input(y)
+# print(x)
+# a = int(input())
+# b = int(input())
+# print(a+b)
+# lst = [1, 2, 3, "4", 5]
+# print(4 in lst, 3 in lst, "4" in lst)
+
+# s = input()
+# numbers = s.split()
+# print(numbers)
+# print(int(numbers[0]) + int(numbers[1]))
+# print("34\t\t4  5\n7".split())
+# print("ab cd hello ".split())
+# s = "12 34"
+# lst = s.split()
+# print(lst)
+
+# s = input()
+# print("  " + s + "\n" + " " + s + s + s + "\n" + s + s + s + s + s)
+# print("  " + s + "\n" + " " + s + s + s + "\n" + s + s + s + s + s + "\n" + " " + s + s + s + "\n" + "  " + s)
+
+# s = input().split()
+# print(s[1])
+# print(s)
+
+# s = input().split()
+# print(float(s[0]) + float(s[1]) + float(s[2]))
+
+# s1 = input()
+# s2 = input()
+# lst = ["NO", "YES"]
+# print(lst[int(s1 in s2)])
+
+# s = input().split()
+# s = list(map(int, s))
+# print((s[0] + s[1]) * s[2])
+
+# s = input()
+# print(s[-1] + s[-2] + s[-3])
+
+# print("""a\nb\tc<
+#       >""")
+
+# s1 = input()
+# s2 = input()
+# lst1 = [s1[0], s1[1], s1[2], s1[3]]
+# lst2 = [s2[0], s2[1], s2[2], s2[3]]
+# lst1[0], lst2[0] = lst2[0], lst1[0]
+# lst1[1], lst2[1] = lst2[1], lst1[1]
+# print("".join(lst1))
+# print("".join(lst2))
+
+# lst = input().S
