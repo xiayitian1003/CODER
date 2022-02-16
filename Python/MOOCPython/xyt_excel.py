e660ad2446e5aadba3f86e648db0e6b90e539d9d
yitian.xia <yitian.xia@transsion.com>
2022-02-16 21:32:57 +0800

    add write in ans-excel code

--- Python/MOOCPython/xyt_excel.py
+++ Python/MOOCPython/xyt_excel.py
@@ -1,20 +1,35 @@
 import openpyxl
-from xythttprequest import search_for_city
+from xythttprequest import pick_point_search_for_city
+from xythttprequest import gaode_search_for_city
 
 Path = "D:\\CODE\\workstation\\Document\\NewestTimeZone.xlsx"
 book = openpyxl.load_workbook(Path)
 sheet = book.worksheets[0]
+write_sheet = book.worksheets[1]
 print(sheet.title)
+print(write_sheet.title)
 print(sheet.min_row, sheet.max_row)
 print(sheet.min_column, sheet.max_column)
+print(write_sheet.min_row, write_sheet.max_row)
+print(write_sheet.min_column, write_sheet.max_column)
 cnt = int(0)
 for row in sheet.rows:
-        cnt += 1
-        print("Start search %s in %d times" % (row[0].value, cnt))
-        ans = search_for_city(row[0].value, row[1].value)
-        if ans is not None:
-            print("%d times finish, ans of %s is %.3f, %.3f" % (cnt, row[0].value, ans['lat'], ans['lon']))
+    cnt += 1
+    print("Start search %s in %d times" % (row[0].value, cnt))
+    ans = pick_point_search_for_city(row[0].value, row[1].value)
+    if ans is not None:
+        print("%d times finish, ans of %s is %.3f, %.3f" % (cnt, row[0].value, ans['lat'], ans['lon']))
+        ans_row = (row[0].value, row[1].value, ans['lat'], ans['lon'])
+        write_sheet.append(ans_row)
+    else:
+        print("%d times finish, ans of %s is None" % (cnt, row[0].value))
+        ans_none_type = "answer in city&country not found"
+        if pick_point_search_for_city(row[0].value, None) is None:
+            ans_lat, ans_lon = "none", "none"
+            ans_none_type = "answer in only city not found"
+            ans_row = (row[0].value, row[1].value, ans_lat, ans_lon, ans['display_name'], ans_none_type)
         else:
-            print("%d times finish, ans of %s is None" % (cnt, row[0].value))
-            # if row[1].value is None:
-            #     print("%3d %10s %10s %10s" % (cnt, row[0].value, row[1].value, row[2].value))
+            print("%3d %10s %10s %10s" % (cnt, row[0].value, row[1].value, row[2].value))
+            ans_row = (row[0].value, row[1].value, ans['lat'], ans['lon'], ans['display_name'], ans_none_type)
+            write_sheet.append(ans_row)
+    book.save(filename="D:\\CODE\\workstation\\Document\\NewestTimeZone.xlsx")
--- Python/MOOCPython/xythttprequest.py
+++ Python/MOOCPython/xythttprequest.py
@@ -1,9 +1,9 @@
 import requests
 
 
-def search_for_city(cityname, countryname):
+def pick_point_search_for_city(cityname, countryname):
     forward_city_url = "https://api.pickpoint.io/v1/forward/"
-    parmas = {'key': "4dhMZBJzyxPyEck7wJey", 'city': cityname}
+    parmas = {'key': "4dhMZBJzyxPyEck7wJey", 'city': cityname, 'country': countryname}
     r = requests.get(forward_city_url, parmas)
     print(r.url)
     print(r.status_code)
@@ -14,10 +14,17 @@ def search_for_city(cityname, countryname):
         ans_dic = r.json()[0]
         print(ans_dic['lat'] + "," + ans_dic['lon'])
         return {'lat': float(ans_dic['lat']), 'lon': float(ans_dic['lon'])}
-        # city_bounding = list(map(float, ans_dic['boundingbox']))
-        # print("%.6f, %.6f" % (city_bounding[0], city_bounding[2]))
-        # print("%.6f, %.6f" % (city_bounding[1], city_bounding[3]))
-        # print((city_bounding[0] + city_bounding[1]) / 2, (city_bounding[2] + city_bounding[3]) / 2)
 
 
-search_for_city("巴林", None)
+def gaode_search_for_city(cityname, countryname):
+    # ?address = 9, Madison Avenue, 曼哈顿, 纽约, 纽约州, 美国 & country = us & language = zh & key = < ⽤户的key >
+    gaode_url = " https://restapi.amap.com/v3/geocode/geo"
+    parmas = {'address': cityname, 'key': "f50480ce1dfb3fd6aa8634f067a559c4", 'country': countryname, 'language': "zh"}
+    r = requests.get(gaode_url, parmas)
+    print(r.url)
+    print(r.status_code)
+    print(r.json())
+
+
+# gaode_search_for_city("巴林", "bh")
+# pick_point_search_for_city("巴林", "巴林")
