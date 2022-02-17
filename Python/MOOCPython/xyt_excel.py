670a22012681e68b1f8ab94c74bb6b9f7704340c
yitian.xia <yitian.xia@transsion.com>
2022-02-17 19:41:12 +0800

    Version 3.0 for successfully search for all city in the correct argument of url-"q"!!!(Stupid me :-( )

--- Python/MOOCPython/xyt_excel.py
+++ Python/MOOCPython/xyt_excel.py
@@ -1,47 +1,77 @@
 import openpyxl
 from xythttprequest import pick_point_search_for_city
+from xythttprequest import gaode_reverse_geo_code
 from xythttprequest import gaode_search_for_city
 
-Path = "D:\\CODE\\workstation\\Document\\NewestTimeZone.xlsx"
+# Path = "D:\\CODE\\workstation\\Document\\NewestTimeZone.xlsx"
+Path = "D:\\CODE\\workstation\\Document\\TimeZone-Version-1.0.xlsx"
 book = openpyxl.load_workbook(Path)
 
-sheet = book.worksheets[0]
-write_sheet = book.create_sheet()
-unfinished_city_sheet = book.create_sheet()
-write_sheet.title = "answer_sheet"
-unfinished_city_sheet.title = "unfinished"
 
-print(sheet.title)
+write_sheet = book.create_sheet()
+
+
 print(write_sheet.title)
-print(sheet.min_row, sheet.max_row)
-print(sheet.min_column, sheet.max_column)
 print(write_sheet.min_row, write_sheet.max_row)
 print(write_sheet.min_column, write_sheet.max_column)
 
-cnt = int(0)
-for row in sheet.rows:
-    cnt += 1
-    print("Start search %s in %d times" % (row[0].value, cnt))
-    ans = pick_point_search_for_city(row[0].value, row[1].value)
-    if ans is not None:
-        print("%d times finish, ans of %s is %.3f, %.3f" % (cnt, row[0].value, ans['lat'], ans['lon']))
-        ans_row = (row[0].value, row[1].value, ans['lat'], ans['lon'], "", ans['display_name'])
-        write_sheet.append(ans_row)
-    else:
-        print("%d times finish, ans of %s is None" % (cnt, row[0].value))
-        ans_none_type = "city&country"
-        ans = pick_point_search_for_city(row[0].value, None)
-        if ans is None:
-            print("city")
-            ans_lat, ans_lon = "none", "none"
-            ans_none_type = "city"
-            ans_row = (row[0].value, row[1].value, ans_lat, ans_lon, ans_none_type, "none name")
-            unfinished_ans = (cnt, row[0].value, row[1].value, ans_lat, ans_lon, ans_none_type, "none name")
+
+def search_all_city():
+    sheet = book.worksheets[0]
+    print(sheet.title)
+    print(sheet.min_row, sheet.max_row)
+    print(sheet.min_column, sheet.max_column)
+    unfinished_city_sheet = book.create_sheet()
+    cnt = int(0)
+    for row in sheet.rows:
+        cnt += 1
+        print("Start search %s in %d times" % (row[0].value, cnt))
+        ans = pick_point_search_for_city(row[0].value, row[1].value)
+        if ans is not None:
+            print("%d times finish, ans of %s is %.3f, %.3f" % (cnt, row[0].value, ans['lat'], ans['lon']))
+            ans_row = (row[0].value, row[1].value, ans['lat'], ans['lon'], "", ans['display_name'])
+            write_sheet.append(ans_row)
         else:
-            print("%3d %10s %10s %10s" % (cnt, row[0].value, row[1].value, row[2].value))
-            ans_row = (row[0].value, row[1].value, ans['lat'], ans['lon'], ans_none_type, ans['display_name'])
-            unfinished_ans = (
-                cnt, row[0].value, row[1].value, ans['lat'], ans['lon'], ans_none_type, ans['display_name'])
-        write_sheet.append(ans_row)
-        unfinished_city_sheet.append(unfinished_ans)
-    book.save(filename=Path)
+            print("%d times finish, ans of %s is None" % (cnt, row[0].value))
+            ans_none_type = "city&country"
+            ans = pick_point_search_for_city(row[0].value, None)
+            if ans is None:
+                print("city")
+                ans_lat, ans_lon = "none", "none"
+                ans_none_type = "city"
+                ans_row = (row[0].value, row[1].value, ans_lat, ans_lon, ans_none_type, "none name")
+                unfinished_ans = (cnt, row[0].value, row[1].value, ans_lat, ans_lon, ans_none_type, "none name")
+            else:
+                print("%3d %10s %10s %10s" % (cnt, row[0].value, row[1].value, row[2].value))
+                ans_row = (row[0].value, row[1].value, ans['lat'], ans['lon'], ans_none_type, ans['display_name'])
+                unfinished_ans = (
+                    cnt, row[0].value, row[1].value, ans['lat'], ans['lon'], ans_none_type, ans['display_name'])
+            write_sheet.append(ans_row)
+            unfinished_city_sheet.append(unfinished_ans)
+        book.save(filename=Path)
+
+
+def search_by_reverse():
+    sheet = book.worksheets[1]
+    print(sheet.title)
+    print(sheet.min_row, sheet.max_row)
+    print(sheet.min_column, sheet.max_column)
+    cnt = int(0)
+    for row in sheet.rows:
+        cnt += 1
+        print("Start search %s in %d times" % (row[0].value, cnt))
+        print("it is --", row[3].value, row[2].value)
+        if row[3].value != "none":
+            ans = gaode_reverse_geo_code(format(float(row[3].value), '.3f'), format(float(row[2].value), '.3f'))
+            ans_row = []
+            plan_to_add = [row[0].value, row[1].value, ans['city'], ans['country']]
+            print(plan_to_add)
+            for item in plan_to_add:
+                if item is not None and len(item) > 0:
+                    ans_row.append(item)
+            write_sheet.append(ans_row)
+            book.save(filename=Path)
+
+
+# search_by_reverse()
+search_all_city()
--- Python/MOOCPython/xythttprequest.py
+++ Python/MOOCPython/xythttprequest.py
@@ -3,7 +3,8 @@ import requests
 
 def pick_point_search_for_city(cityname, countryname):
     forward_city_url = "https://api.pickpoint.io/v1/forward/"
-    parmas = {'key': "4dhMZBJzyxPyEck7wJey", 'city': cityname, 'country': countryname}
+    parmas = {'key': "4dhMZBJzyxPyEck7wJey", 'q': cityname, 'city': cityname, 'country': countryname,
+              'accept-language': "zh-CN"}
     r = requests.get(forward_city_url, parmas)
     print(r.url)
     print(r.status_code)
@@ -16,6 +17,21 @@ def pick_point_search_for_city(cityname, countryname):
         return {'lat': float(ans_dic['lat']), 'lon': float(ans_dic['lon']), 'display_name': ans_dic['display_name']}
 
 
+# 26.1551249, 50.5344606
+def pick_point_reverse_geo_code(lat, lon):
+    # https://restapi.amap.com/v3/geocode/regeo?output=xml&location=116.310003,39.991957&key=<用户的key>&radius=1000&extensions=all
+    url = "https://api.pickpoint.io/v1/reverse/"
+    pick_point_key = "4dhMZBJzyxPyEck7wJey"
+    parmas = {'key': pick_point_key, 'lat': lat, 'lon': lon, 'accept-language': "zh-CN", 'namedetails': 1}
+    r = requests.get(url, parmas)
+    print(r.status_code)
+    ans = r.json()
+    print(ans)
+    # return {'formatted_address': ans['regeocode']['formatted_address'],
+    #         'city': ans['regeocode']['addressComponent']['city'],
+    #         'country': ans['regeocode']['addressComponent']['country']}
+
+
 def gaode_search_for_city(cityname, countryname):
     # ?address = 9, Madison Avenue, 曼哈顿, 纽约, 纽约州, 美国 & country = us & language = zh & key = < ⽤户的key >
     gaode_url = " https://restapi.amap.com/v3/geocode/geo"
@@ -26,5 +42,21 @@ def gaode_search_for_city(cityname, countryname):
     print(r.json())
 
 
+def gaode_reverse_geo_code(lon, lat):
+    # https://restapi.amap.com/v3/geocode/regeo?output=xml&location=116.310003,39.991957&key=<用户的key>&radius=1000&extensions=all
+    url = "https://restapi.amap.com/v3/geocode/regeo"
+    gaode_key = "86959160eb68570d15ae3236db7aa989"
+    parmas = {'location': str(lon) + "," + str(lat), 'key': gaode_key}
+    r = requests.get(url, parmas)
+    print(r.status_code)
+    ans = r.json()
+    print(ans)
+    return {'formatted_address': ans['regeocode']['formatted_address'],
+            'city': ans['regeocode']['addressComponent']['city'],
+            'country': ans['regeocode']['addressComponent']['country']}
+
+
+# gaode_reverse_geo_code(18.5509126, 4.3907153)
 # gaode_search_for_city("巴林", "bh")
-# pick_point_search_for_city("巴林", "巴林")
+# pick_point_search_for_city("巴林", None)
+# pick_point_reverse_geo_code(26.1551249, 50.5344606)
