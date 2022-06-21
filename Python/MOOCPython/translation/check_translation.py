db64bbbe1cae91ab8fb9c556497d1f0ce1ded262
yitian.xia <yitian.xia@transsion.com>
2022-06-21 15:44:12 +0800

    add python file of checking English translation of cities write it into Excel

--- Python/MOOCPython/translation/check_translation.py
+++ Python/MOOCPython/translation/check_translation.py
@@ -6,15 +6,15 @@ from xml.dom.minidom import parse, Node
 def main():
     eng_excel_dir = "D:\\CODE\\workstation\\Document\\9.0\\9.0提翻译\\ENWorldcities.xlsx"
     book = openpyxl.load_workbook(eng_excel_dir)
-    sheet = book.worksheets[0]
+    sheet = book.worksheets[1]
     city_lst = []
     for row in sheet.rows:
         city_lst.append(row[0].value)
-
+    #
     # for item in city_lst:
     #     print(item)
     print("start check")
-
+    #     <!--add by xiaolei.huang  start-->
     eng_xml_dir = "D:\\CODE\\workstation\\Document\\9.0\\9.0提翻译\\cities.xml"
     write_xml_path = "D:\\CODE\\workstation\\Document\\9.0\\9.0提翻译\\rightENcities.xml"
     dom = parse(write_xml_path)
